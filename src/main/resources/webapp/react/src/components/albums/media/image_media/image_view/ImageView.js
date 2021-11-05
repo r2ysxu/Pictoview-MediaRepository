756bc56e6e2808cd10f77747f9289472ae121827
r2ysxu <r2ysxu@github.com>
2021-11-05 01:47:28 -0700

    Fixed full images not showing up

--- src/main/resources/webapp/react/src/components/albums/media/image_media/image_view/ImageView.js
+++ src/main/resources/webapp/react/src/components/albums/media/image_media/image_view/ImageView.js
@@ -4,19 +4,19 @@ import './ImageView.css';
 
 function ImageView({imageItems, selectedIndex, onSelectIndex}) {
 
-	const onCloseModal = () => {
-		onSelectIndex(null);
-	}
+    const onCloseModal = () => {
+        onSelectIndex(null);
+    }
 
-	return ((selectedIndex != null && selectedIndex >= 0) ?
-		<div className="image_view_container">
-			<div className="image_view_image_container">
-	            <div className="image_view_container_modal" onClick={onCloseModal}></div>
-				<img className="image_view_image" src={'/album/image/full?mediaid=' + imageItems[selectedIndex].id} alt={imageItems[selectedIndex].name} title={imageItems[selectedIndex]} />
-	        </div>
-	        <ImageViewControls imageCount={imageItems.length} selectedIndex={selectedIndex} onIndexChange={onSelectIndex} />
-		</div> : <div />
-	);
+    return ((selectedIndex != null && selectedIndex >= 0) ?
+        <div className="image_view_container">
+            <div className="image_view_content">
+                <div className="image_view_container_modal" onClick={onCloseModal}></div>
+                <img className="image_view_image" src={'/album/image/full?mediaid=' + imageItems[selectedIndex].id} alt={imageItems[selectedIndex].name} title={imageItems[selectedIndex].name} />
+                <ImageViewControls imageCount={imageItems.length} selectedIndex={selectedIndex} onIndexChange={onSelectIndex} />
+            </div>
+        </div> : <div />
+    );
 }
 
 export default ImageView;
