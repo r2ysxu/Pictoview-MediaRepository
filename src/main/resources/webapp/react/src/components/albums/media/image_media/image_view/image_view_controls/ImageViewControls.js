b28b4938f15f0cb6ace6d6b222fadf276ab2f7f1
r2ysxu <r2ysxu@github.com>
2021-11-06 03:25:59 -0700

    Added arrow key controls to imageview

--- src/main/resources/webapp/react/src/components/albums/media/image_media/image_view/image_view_controls/ImageViewControls.js
+++ src/main/resources/webapp/react/src/components/albums/media/image_media/image_view/image_view_controls/ImageViewControls.js
@@ -1,8 +1,35 @@
 import React from 'react';
-import { useState } from 'react';
+import { useEffect, useCallback } from 'react';
 import './ImageViewControls.css';
 
 function ImageViewControls({imageCount, selectedIndex, onIndexChange, onChangeCoverImage}) {
+
+    const onNextIndex = () => {
+        if (selectedIndex > 0) onIndexChange(selectedIndex - 1);
+    }
+
+    const onPrevIndex = () => {
+        if (selectedIndex < imageCount - 1) onIndexChange(selectedIndex + 1);
+    }
+
+    const onCancel = () => {
+        onIndexChange(null);
+    }
+
+    const useKeyControls = useCallback( event => {
+        if (event.keyCode === 39) { // Left Arrow
+            onPrevIndex();
+        } else if (event.keyCode === 37) { // Right Arrow
+            onNextIndex();
+        } else if (event.keyCode === 39 || event.keyCode === 40) { // Up or Down Arrow
+            onIndexChange(null);
+        }
+    }, [selectedIndex, onIndexChange]);
+
+    useEffect( () => {
+        document.addEventListener('keyup', useKeyControls);
+        return () => document.removeEventListener('keyup', useKeyControls);
+    }, [useKeyControls]);
 
     return (
         <>
@@ -17,17 +44,13 @@ function ImageViewControls({imageCount, selectedIndex, onIndexChange, onChangeCo
             <div className="image_view_controls_container">
                 <img className={'image_vie_controls_icons image_view_controls_icons_small' + (selectedIndex > 0 ? ' ' : ' image_view_controls_hidden')}
                     src="/assets/icons/arrow-left-circle.svg" alt=""
-                    onClick={() => {
-                        if (selectedIndex > 0) onIndexChange(selectedIndex - 1);
-                    }} />
+                    onClick={onNextIndex} />
                 <img className="image_view_controls_icons image_view_controls_icons_large"
                     src="/assets/icons/x-circle-fill.svg" alt=""
-                    onClick={() => onIndexChange(null)}/>
+                    onClick={onCancel}/>
                 <img className={'image_view_controls_icons image_view_controls_icons_small' + (selectedIndex < imageCount - 1 ? ' ' : ' image_view_controls_hidden')}
                     src="/assets/icons/arrow-right-circle.svg" alt=""
-                    onClick={() => {
-                        if (selectedIndex < imageCount - 1) onIndexChange(selectedIndex + 1);
-                    }} />
+                    onClick={onPrevIndex} />
             </div>
         </>
     );
