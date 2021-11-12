b3ae78ca96ee026fd6333c45df4cbbb80d690e8c
r2ysxu <r2ysxu@github.com>
2021-11-11 21:03:39 -0800

    Reload on unauthorized user

--- src/main/java/org/mrn/exceptions/UnauthenticatedUserException.java
+++ src/main/java/org/mrn/exceptions/UnauthenticatedUserException.java
@@ -1,5 +1,9 @@
 package org.mrn.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 public class UnauthenticatedUserException extends Exception {
 	private static final long serialVersionUID = 1L;
 
--- src/main/resources/webapp/react/src/model/apis/album_apis.js
+++ src/main/resources/webapp/react/src/model/apis/album_apis.js
@@ -1,35 +1,42 @@
+const onResponseJson = (response) => {
+    if (response.status === 401) {
+        window.location.reload();
+    }
+    return response.json();
+}
+
 export const get_fetchAlbums = async (page, parentId) => {
     const searchParams = new URLSearchParams({page, parentId});
-    return fetch('/album/list?' + searchParams.toString()).then( response => response.json());
+    return fetch('/album/list?' + searchParams.toString()).then(onResponseJson);
 }
 
 export const get_searchAlbums = async (page, query) => {
     const searchParams = new URLSearchParams({page, query});
-    return fetch('/album/search?' + searchParams.toString()).then( response => response.json());
+    return fetch('/album/search?' + searchParams.toString()).then(onResponseJson);
 }
 
 export const get_listAlbumImages = async (page, albumId) => {
     if (albumId <= 0) return { items: [], pageInfo: { page: 0, total: 0, hasNext: false } };
     const searchParams = new URLSearchParams({page, albumId});
-    return fetch('/album/images/list?' + searchParams.toString()).then( response => response.json());
+    return fetch('/album/images/list?' + searchParams.toString()).then(onResponseJson);
 }
 
 export const get_listAlbumVideos = async (page, albumId) => {
     if (albumId <= 0) return { items: [], pageInfo: { page: 0, total: 0, hasNext: false } };
     const searchParams = new URLSearchParams({page, albumId});
-    return fetch('/album/videos/list?' + searchParams.toString()).then( response => response.json());
+    return fetch('/album/videos/list?' + searchParams.toString()).then(onResponseJson);
 }
 
 export const get_listAlbumAudios = async (page, albumId) => {
     if (albumId <= 0) return { items: [], pageInfo: { page: 0, total: 0, hasNext: false } };
     const searchParams = new URLSearchParams({page, albumId});
-    return fetch('/album/audio/list?' + searchParams.toString()).then( response => response.json());
+    return fetch('/album/audio/list?' + searchParams.toString()).then(onResponseJson);
 }
 
 export const get_listAlbumTags = async(albumId) => {
     if (albumId <= 0) return {};
     const searchParams = new URLSearchParams({albumId});
-    return fetch('/album/tag/list?' + searchParams.toString()).then( response => response.json());
+    return fetch('/album/tag/list?' + searchParams.toString()).then(onResponseJson);
 }
 
 export const post_createAlbum = async(album) => {
@@ -37,7 +44,7 @@ export const post_createAlbum = async(album) => {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(album),
-    }).then( response => response.json());
+    }).then(onResponseJson);
 }
 
 export const post_uploadAlbum = async(albumId, file, fromMetadata) => {
@@ -49,7 +56,7 @@ export const post_uploadAlbum = async(albumId, file, fromMetadata) => {
         method: 'POST',
         headers: { 'enctype': 'multipart/form-data' },
         body: formData,
-    }).then( response => response.json());
+    }).then(onResponseJson);
 }
 
 export const post_updateAlbum = async(album) => {
@@ -57,7 +64,7 @@ export const post_updateAlbum = async(album) => {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(album),
-    }).then( response => response.json());
+    }).then(onResponseJson);
 }
 
 export const post_changeAlbumCover = async(albumId, imageId) => {
@@ -65,5 +72,5 @@ export const post_changeAlbumCover = async(albumId, imageId) => {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({albumId, imageId}),
-    }).then( response => response.json());
+    }).then(onResponseJson);
 }
