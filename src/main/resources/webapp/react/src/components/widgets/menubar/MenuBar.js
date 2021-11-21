091c496ab948d142a8e0842f4853298e9c33ed68
r2ysxu <r2ysxu@github.com>
2021-11-21 00:40:51 -0800

    Added hide menu on unfocus

--- src/main/resources/webapp/react/src/components/widgets/menubar/MenuBar.js
+++ src/main/resources/webapp/react/src/components/widgets/menubar/MenuBar.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './MenuBar.css';
 
 function MenuBar({children, lastItem, selectedIndex, onSelect}) {
+    const ref = useRef();
     const [showExpand, setShowExpand] = useState(false);
+
+    useEffect(() => {
+        const onOutsideClicked = (event) => {
+            if (ref.current && !ref.current.contains(event.target)) {
+                setShowExpand(false);
+            }
+        };
+        document.addEventListener("mousedown", onOutsideClicked);
+        return () => document.removeEventListener("mousedown", onOutsideClicked);
+    }, [setShowExpand]);
+
     return (
-        <div className="menubar_container">
+        <div className="menubar_container" ref={ref}>
             <div className="menubar_icon" onClick={() => setShowExpand(!showExpand)}>
                 <img className="searchbar_iv_icon" src="/assets/icons/list.svg" alt="" />
             </div>
