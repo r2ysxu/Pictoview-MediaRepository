4550de5710e51df7d335e2d4110b8b54fbba6bdd
r2ysxu <r2ysxu@github.com>
2021-11-24 19:54:21 -0800

    Added Tomcat deploy support

--- src/main/java/org/mrn/PvrnApplication.java
+++ src/main/java/org/mrn/PvrnApplication.java
@@ -3,25 +3,32 @@ package org.mrn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 @EnableJpaRepositories("org.mrn.jpa.repo")
-@EntityScan({"org.mrn.jpa.model", "org.mrn.service.builder"})
+@EntityScan({ "org.mrn.jpa.model", "org.mrn.service.builder" })
 @ComponentScan
 @SpringBootApplication
-public class PvrnApplication {
+public class PvrnApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PvrnApplication.class, args);
 	}
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PvrnApplication.class);
+    }
+
 	@Bean(name = "multipartResolver")
 	public CommonsMultipartResolver multipartResolver() {
-	    CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-	    multipartResolver.setMaxUploadSize(-1);
-	    return multipartResolver;
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setMaxUploadSize(-1);
+		return multipartResolver;
 	}
 }
