1d3e5c7572419e8e41a296633affeb679918d709
r2ysxu <r2ysxu@gmail.com>
2021-12-10 17:07:04 -0800

    Added simple caching to video resources

--- src/main/java/org/mrn/service/VideoService.java
+++ src/main/java/org/mrn/service/VideoService.java
@@ -1,5 +1,7 @@
 package org.mrn.service;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.transaction.Transactional;
 
 import org.mrn.jpa.model.album.VideoMediaEntity;
@@ -10,10 +12,19 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
 import reactor.core.publisher.Mono;
 
 @Service
 public class VideoService {
+	
+	Cache<String, Resource> resourceCache = CacheBuilder
+			.newBuilder()
+			.maximumSize(10)
+			.expireAfterAccess(4L, TimeUnit.HOURS)
+			.build();
 
 	@Autowired
 	private ResourceLoader resourceLoader;
@@ -21,7 +32,14 @@ public class VideoService {
 	private VideoMediaRepo videoMediaRepo;
 
 	public Mono<Resource> fetchVideoStream(UserDetails owner, Long mediaId) {
-		return Mono.fromSupplier(() -> resourceLoader.getResource("file:" + findVideo(owner, mediaId).getSource()));
+		Resource resource = resourceCache.getIfPresent(mediaId + "_" + owner.getUsername());
+		if (resource == null) {
+			final Resource unCachedResource = resourceLoader.getResource("file:" + findVideo(owner, mediaId).getSource());
+			resourceCache.put(mediaId + "_" + owner.getUsername(), unCachedResource);
+			return Mono.fromSupplier(() -> unCachedResource);
+		} else {
+			return Mono.fromSupplier(() -> resource);
+		}
 	}
 
 	@Transactional
