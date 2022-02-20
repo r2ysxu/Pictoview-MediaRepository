281861b2428363fba5da23d74a5e0610bbc03bbd
r2ysxu <r2ysxu@gmail.com>
2022-02-20 02:53:46 -0800

    Added audio slider styling

--- src/main/resources/webapp/react/src/components/albums/media/audio_media/audio_view/AudioView.js
+++ src/main/resources/webapp/react/src/components/albums/media/audio_media/audio_view/AudioView.js
@@ -4,16 +4,18 @@ import './AudioView.css';
 
 function AudioView({audioItems, selectedIndex}) {
     const audioPlayerRef = useRef();
+    const audioSliderRef = useRef();
     const [audioDuration, setAudioDuration] = useState(0);
+    const [audioCurrentTime, setAudioCurrentTime] = useState(0);
+    const [audioTotalTime, setAudioTotalTime] = useState(0);
+    const [isPaused, setPaused] = useState(true);
 
     const MAX_VALUE = 100;
 
     const onChangeTrackTime = (event) => {
         const audioPlayer = audioPlayerRef.current;
         const percentage = event.target.value;
-        const newTime = Math.floor(percentage * audioPlayer.duration / 100);
-        setAudioDuration(newTime);
-        audioPlayer.currentTime = newTime;
+        audioPlayer.currentTime = Math.floor(percentage * audioPlayer.duration / 100);
     }
 
     useEffect( () => {
@@ -21,33 +23,44 @@ function AudioView({audioItems, selectedIndex}) {
             if (audioPlayer === null || audioPlayer === undefined) return;
             audioPlayer.load();
             audioPlayer.play();
+            setPaused(false);
         }
 
-        const displayBuffered = (audio) => {
-            const bufferedAmount = Math.floor(audio.buffered.end(audio.buffered.length - 1));
+        const displayBuffered = (audioPlayer, audioSlider) => {
+            const bufferedAmount = Math.floor(audioPlayer.buffered.end(audioPlayer.buffered.length - 1));
             console.log(bufferedAmount);
-            audio.style.setProperty('--buffered-width', `${(bufferedAmount / 100) * 100}%`);
+            audioSlider.style.setProperty('--buffered-width', `${(bufferedAmount / 100) * 100}%`);
         }
 
-        const displayTrack = (audioPlayer) => {
+        const displayTrack = (audioPlayer, audioSlider) => {
             if (audioPlayer === null || audioPlayer === undefined) return;
             audioPlayer.addEventListener('loadedmetadata', () => {
-                displayBuffered(audioPlayer);
+                displayBuffered(audioPlayer, audioSlider);
             });
             audioPlayer.addEventListener('timeupdate', () => {
-                setAudioDuration(Math.floor(audioPlayer.currentTime * MAX_VALUE / audioPlayer.duration) || 0);
+                const percentage = Math.floor(audioPlayer.currentTime * MAX_VALUE / audioPlayer.duration) || 0;
+                setAudioDuration(percentage);
+                setAudioTotalTime(audioPlayer.duration);
+                setAudioCurrentTime(audioPlayer.currentTime);
+                audioSlider.style.setProperty('--seek-before-width', `${percentage}%`);
             });
         }
 
         const audioPlayer = audioPlayerRef.current;
+        const audioSlider = audioSliderRef.current;
         onChangeTrack(audioPlayer);
-        displayTrack(audioPlayer);
+        displayTrack(audioPlayer, audioSlider);
     }, [audioPlayerRef, selectedIndex]);
 
     const onTogglePlay = () => {
         const audioPlayer = audioPlayerRef.current;
-        if (audioPlayer.paused) audioPlayer.play();
-        else audioPlayer.pause();
+        if (audioPlayer.paused) {
+            audioPlayer.play();
+            setPaused(false);
+        } else {
+            audioPlayer.pause();
+            setPaused(true);
+        }
     }
 
     return ((selectedIndex != null && selectedIndex >= 0 && audioItems[selectedIndex]) ?
@@ -55,11 +68,19 @@ function AudioView({audioItems, selectedIndex}) {
             <div className="audio_view_audio_info">
             </div>
             <div className="audio_view_audio_content_container">
-                <audio className="audio_view_audio_tag" autoPlay ref={audioPlayerRef}>
-                    <source src={'/album/audio?mediaid=' + audioItems[selectedIndex].id} />
-                </audio>
-                    <input type="image" src="/assets/icons/play-circle.svg" onClick={onTogglePlay} />
-                    <input type="range" onChange={onChangeTrackTime} max={MAX_VALUE} value={audioDuration} />
+                <div className="audio_view_content_slider">
+                    <audio className="audio_view_audio_tag" autoPlay ref={audioPlayerRef}>
+                        <source src={'/album/audio?mediaid=' + audioItems[selectedIndex].id} />
+                    </audio>
+                    <input type="range" onChange={onChangeTrackTime} max={MAX_VALUE} value={audioDuration} ref={audioSliderRef} />
+                </div>
+                <div class="audio_view_audio_content_left_info">
+                    <span>{audioCurrentTime}</span>
+                </div>
+                <div class="audio_view_audio_content_right_info">
+                    <span>{audioTotalTime}</span>
+                </div>
+                <input className="audio_view_content_play_button" type="image" src={isPaused ? "/assets/icons/play-circle.svg" : "/assets/icons/pause-circle.svg"} onClick={onTogglePlay} />
             </div>
         </div> : <div />
     );
