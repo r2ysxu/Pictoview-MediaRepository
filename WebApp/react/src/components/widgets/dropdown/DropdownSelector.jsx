8e6208312f63a7fc3c16bd4956a908a18b5eb99e
r2ysxu <r2ysxu@gmail.com>
2023-06-12 14:15:34 -0700

    Fixed dropdown

--- WebApp/react/src/components/widgets/dropdown/DropdownSelector.jsx
+++ WebApp/react/src/components/widgets/dropdown/DropdownSelector.jsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './styles.css';
 
 function DropdownSelector({ setSelectedValueItem, onSelect, values, onClose, width, className = "" }) {
+  const ref = useRef();
+
+  useEffect(() => {
+    const onOutsideClicked = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        onClose();
+      }
+    };
+    document.addEventListener("mousedown", onOutsideClicked);
+    return () => document.removeEventListener("mousedown", onOutsideClicked);
+  });
+
+  const onHandleClose = () => {
+    onClose();
+  }
+
   return (
-    <div className="dropdown_float_container" style={{ width, minWidth: width }}>
-      {values.map( (value, index) => 
-        <div key={index} className={`dropdown_float_item ${className}`}
-          onClick={() => {
-            setSelectedValueItem(values[index]);
-            onSelect(values[index]);
-            onClose();
-          }}>
-          {value.name}
-        </div>
-      )}
-    </div>
+    <>
+      <div className="dropdown_modal" onClick={onHandleClose} />
+      <div className="dropdown_float_container" ref={ref}>
+        {values.map( (value, index) => 
+          <div key={index} className={`dropdown_float_item ${className}`}
+            onClick={() => {
+              setSelectedValueItem(values[index]);
+              onSelect(values[index]);
+              onClose();
+            }}>
+            {value.name}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
--- WebApp/react/src/components/widgets/dropdown/index.jsx
+++ WebApp/react/src/components/widgets/dropdown/index.jsx
@@ -12,7 +12,7 @@ function Dropdown({selectedValue = null, onSelect, values, placeholder, width, c
     <div className={`dropdown_container ${className}`}>
       <div className="dropdown_selector_container" style={{ width, minWidth: width }}
           onClick={() => setShowSelector(!showSelector)}>
-        <img className="dropdown_icon" src={"/assets/icons/"+ (showSelector ? "chevron-up.svg" : "chevron-down.svg")} alt="dropdown" />
+        <img className="dropdown_chevron" src={"/assets/icons/"+ (showSelector ? "chevron-up.svg" : "chevron-down.svg")} alt="dropdown" />
         {selectedValueItem === null ?
           <span className="dropdown_selector_placeholder_text"><i>{placeholder}</i></span> :
           <>
